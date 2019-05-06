4181155d0d7d315bbbd0bcdb64628d9bd8647fa2
Shahzaib <shahzaibrocks9@gmail.com>
2019-05-06 23:15:15 +0500

    Rocket v2

--- Rocket.c
+++ Rocket.c
@@ -4,118 +4,159 @@
 void delay(int number_of_seconds);//function for yime delay
 
 int main(int argc, char const *argv[]) {
-		int slash,row,space,stars,line,i;
+		int slash,row,space,stars,line,i,cols;
 		//bring the cursor to the bottom
-		for (i=0; i<100; i++)
-				printf("\n" );
+		FILE *fp;//for getting terminal windows status
+		char var[40];
+		fp = popen("mode con /status ", "r");//fp = popen("tput cols", "r");
+		//For unix based system use
+		//tput cols
+		//tput lines
+		while (fgets(var, sizeof(var), fp)!= NULL)
+		{
+				printf("%s",var);
+		}
+		pclose(fp);
+		printf("Enter the no of columns\n");
+		scanf("%d",&cols);
+		for (i=0; i<50; i++)
+				putchar('\n' );
 
 		//for printing the upper part of rocket
 		for(row=0; row<5; row++)
 		{
-
-				printf("\t\t\t\t\t\t");//bring the cursor at the center of screen
-				/*I know its not the best way to move the cusor suggest any alternative function*/
+				for(i=0; i<(cols/2); i++)
+						putchar(' ');
 				for(space=0; space<5-row; space++)
-						printf(" ");
+						putchar(' ');
 				for(slash=0; slash<=row; slash++)
 				{
-						printf("/");
+						putchar('/');
 				}
-				printf("*");
+				putchar('*');
 				for (slash=0; slash<=row; slash++)
 				{
-						printf("\\");
+						putchar('\\');
 				}
-				printf("\n");
+				putchar('\n');
 		}
-		printf("\t\t\t\t\t\t");
+		for(i=0; i<(cols/2); i++)
+				putchar(' ');
 		for(stars=0; stars<=5; stars++)
 		{
-				printf("*=");
+				putchar('*'); putchar('=');
 		}
-		printf("+\n");
-		printf("\t\t\t\t\t\t|-----------|\n");
+		putchar('+'); putchar('\n');
+		for(i=0; i<(cols/2); i++)
+				putchar(' ');
+		for(i=0; i<13; i++)
+		{
+				if(i==0||i==12)
+						putchar('|');
+				else
+						putchar('-');
+		}
+		putchar('\n');
 		//middle rocket
 		for(line=0; line<10; line++)
 		{
-				if (line==5)
+				for(i=0; i<(cols/2); i++)
+						putchar(' ');
+				putchar('|');
+				for(i=0; i<11; i++)
 				{
-						printf("\t\t\t\t\t\t|_._. N ._._|\n"); line++;
-						printf("\t\t\t\t\t\t|_._. A ._._|\n"); line++;
-						printf("\t\t\t\t\t\t|_._. S ._._|\n"); line++;
-						printf("\t\t\t\t\t\t|_._. A ._._|\n"); line++;
+						if(line==4&&i==4)
+						{
+								putchar('N'); i++;
+								putchar('S'); i++;
+								putchar('F'); i++;
+						}
+						putchar(' ');
 				}
-				printf("\t\t\t\t\t\t|_._._._._._|");
-				printf("\n" );
+				putchar('|');
+				putchar('\n');
 		}
-
-		printf("\t\t\t\t\t\t|___________|\n");
-
-
-		printf("\t\t\t\t\t\t");
-		for(stars=0; stars<=5; stars++)
+		for(i=0; i<(cols/2); i++)
+				putchar(' ');
+		for(i=0; i<13; i++)
 		{
-				printf("*=");
+				if(i==0||i==12)
+						putchar('|');
+				else
+						putchar('_');
 		}
-		printf("+\n");
-		//end part of the rocket
+		putchar('\n');
+		// bottom part
+		delay(1);
 		for(row=0; row<5; row++)
 		{
-
-				printf("\t\t\t\t\t\t");//bring the cursor at the center of screen
-				/*I know its not the best way to move the cusor suggest any alternative function*/
+				for(i=0; i<(cols/2); i++)
+						putchar(' ');
 				for(space=0; space<5-row; space++)
-						printf(" ");
+						putchar(' ');
 				for(slash=0; slash<=row; slash++)
 				{
-						printf("/");
+						putchar('/');
 				}
-				printf("*");
+				putchar('*');
 				for (slash=0; slash<=row; slash++)
 				{
-						printf("\\");
+						putchar('\\');
 				}
-				delay(1);
-				printf("\n");
+				putchar('\n');
 		}
-		delay(1);
 		//for thrust
-		for (stars=0; stars<10; stars++)
-		{
-				printf("\t\t\t\t\t\t ************\n" );
-				delay(1);
-		}
-		for (stars=0; stars<8; stars++)
-		{
-				printf("\t\t\t\t\t\t   ********\n");
-		}
-		for (stars=0; stars<7; stars++)
-		{
-				printf("\t\t\t\t\t\t     ****\n");
-				delay(1);
-		}
-		for(stars=0; stars<4; stars++)
-		{
-				printf("\t\t\t\t\t\t      **\n");
-		}
 		delay(1);
-		//removing the thrust
-		for(line=0; line<30; line++)
+		for(i=0; i<30; i++)
 		{
-			if(line==15)
-			{
-				printf("\t\t\t\t\t\tRocket has Gone!\n");
-			}
-				printf("\n");
+				for(space=0; space<(cols/2); space++)
+						putchar(' ');
+				if(i<8)
+				{
+						for(stars=13; stars>0; stars--)
+						{
+								putchar('*');
+						}
+				}
+				else if(i>8&&i<15)
+				{
+						for(space=0; space<3; space++)
+								putchar(' ');
+						for(stars=8; stars>0; stars--)
+						{
+								putchar('*');
+						}
+				}
+				else if(i>15&&i<25)
+				{
+						for(space=0; space<5; space++)
+								putchar(' ');
+						for(stars=4; stars>0; stars--)
+						{
+								putchar('*');
+						}
+				}
+				else if(i>25)
+				{
+						for(space=0; space<6; space++)
+								putchar(' ');
+						for(stars=2; stars>0; stars--)
+						{
+								putchar('*');
+						}
+				}
+				putchar('\n');
+				delay(1);
 		}
+		for(i=0; i<50; i++)
+				putchar('\n');
 		return 0;
 }
-
 //delay function
 void delay(int number_of_seconds)
 {
 		// Converting time into milli_seconds
-		int milli_seconds = 400 * number_of_seconds;
+		int milli_seconds = 200 * number_of_seconds;
 
 		// Stroing start time
 		clock_t start_time = clock();
